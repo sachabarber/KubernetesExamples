147e2edb813408a691039181dea2bde980bd9985
sacha barber <sbarber@nephilaadvisors.co.uk>
2018-02-14 19:00:11 +0000

    working on 2nd part

---
+++ Post2_SimpleServiceStackPod/sswebapp/src/sswebapp.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceStack;
+using sswebapp.ServiceModel;
+
+namespace sswebapp.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
--- Post2_SimpleServiceStackPod/sswebapp/src/sswebapp/Program.cs
+++ Post2_SimpleServiceStackPod/sswebapp/src/sswebapp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace SimpleWebApiPod
+namespace sswebapp
 {
     public class Program
     {
@@ -19,7 +19,6 @@ namespace SimpleWebApiPod
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:80")
                 .UseStartup<Startup>()
                 .Build();
     }
