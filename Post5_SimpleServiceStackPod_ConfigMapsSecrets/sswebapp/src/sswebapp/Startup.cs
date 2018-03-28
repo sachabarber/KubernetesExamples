18b0ea8225cc0a80b25dc6f51376b2402422cc2f
sacha barber <sbarber@nephilaadvisors.co.uk>
2018-03-28 21:57:14 +0100

    Working on post5

---
+++ Post5_SimpleServiceStackPod_ConfigMapsSecrets/sswebapp/src/sswebapp/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Funq;
+using ServiceStack;
+using ServiceStack.Configuration;
+using sswebapp.ServiceInterface;
+using ServiceStack.Api.Swagger;
+using System.Text;
+
+namespace sswebapp
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("/k8s/config/sachaserver-properties", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            //Set the Static on MyServices, which is very poor design, but it is just for a
+            //demo so I am letting it slide
+            MyServices.AllVariablesFromStartup = Configuration.AsEnumerable();
+
+            app.UseServiceStack(new AppHost
+            {
+                AppSettings = new NetCoreAppSettings(Configuration)
+            });
+        }
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("sswebapp", typeof(MyServices).Assembly) { }
+
+        // Configure your AppHost with the necessary configuration and dependencies your App needs
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new SwaggerFeature());
+
+            SetConfig(new HostConfig
+            {
+                DefaultRedirectPath = "/metadata",
+                DebugMode = AppSettings.Get(nameof(HostConfig.DebugMode), false)
+            });
+        }
+    }
+}
